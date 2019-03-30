2bd0beaa63bb374bee54820b0f01e2879e7c516f
Valer Malin <mdfcnvsn@gmail.com>
2019-03-30 23:31:47 +0200

    added task3

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -12,6 +12,7 @@ namespace ConsoleApp
         {
             Task1();
             Task2();
+            Task3();
         }
         static void Task1()
         {
@@ -27,6 +28,12 @@ namespace ConsoleApp
             const decimal E = 2.7182818284590452m;
             Console.WriteLine($"число Эйлера {E} число Pi {Pi}");
             Console.ReadKey();
+        }
+        static void Task3()
+        {
+            string firstStr = "\nмоя строка1";
+            string secodStr = "\tмоя строка2";
+            string thirdStr = "\aмоя строка3";
         }
 
 
