040e72b94a646a201efc9105b090e2c04993a151
Valer Malin <mdfcnvsn@gmail.com>
2019-03-30 14:07:04 +0200

    variables fixed

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int uberflu = 1;
+            int Identifier = 2;
+            int \u006fIdentifier = 3;
+            int myVar = 4;
+            int myVariable = 5;
+
+
+
+
+        }
+    }
+}
