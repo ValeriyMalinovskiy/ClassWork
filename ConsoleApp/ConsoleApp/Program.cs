1de7c4b06b6f5ea4a596b8beb84befa892d8d683
Valer Malin <mdfcnvsn@gmail.com>
2019-03-31 00:14:55 +0200

    added task4 in new branch

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -10,6 +10,17 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
+            Task4();
+        }
+        static void Task4()
+        {
+            short num1 = 10;
+            short num2 = 15;
+            Console.WriteLine($"{num1}+{num2}={Program.Sum(num1, num2)}");
+        }
+        static int Sum(int x,int y)
+        {
+            return x + y;
         }
     }
 }
