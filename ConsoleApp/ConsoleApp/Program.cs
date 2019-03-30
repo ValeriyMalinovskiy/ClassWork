15f0f9917fc7cc53dfc2ef10e01b82bf53c247af
Valer Malin <mdfcnvsn@gmail.com>
2019-03-31 00:19:42 +0200

    added task4

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -14,8 +14,8 @@ namespace ConsoleApp
         }
         static void Task4()
         {
-            short num1 = 10;
-            short num2 = 15;
+            short num1 = short.Parse(Console.ReadLine());
+            short num2 = short.Parse(Console.ReadLine());
             Console.WriteLine($"{num1}+{num2}={Program.Sum(num1, num2)}");
         }
         static int Sum(int x,int y)
