9f6de6c7d444330ee641253eeca9cb6655a668b1
Valer Malin <mdfcnvsn@gmail.com>
2019-03-30 23:29:13 +0200

    Added task1

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -10,6 +10,19 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
+            Task1();
         }
+        static void Task1()
+        {
+            int uberflu = 1;
+            int _Identifier = 2;
+            int \u006fIdentifier = 3;
+            int myVar = 4;
+            int myVariable = 5;
+        }
+
+
+
+
     }
 }
