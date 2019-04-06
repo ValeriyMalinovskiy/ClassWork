2cea37dd445a29f3d771a6eb40a684f8418b5ae0
Valer Malin <mdfcnvsn@gmail.com>
2019-04-06 11:04:44 +0300

    Removed previous classwork branches

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -10,36 +10,6 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
-            Task1();
-            
-            Task2();
-            
-            Task3();
-        }
-        static void Task1()
-        {
-            int uberflu = 1;
-            int _Identifier = 2;
-            int \u006fIdentifier = 3;
-            int myVar = 4;
-            int myVariable = 5;
-        }
-        static void Task2()
-        {
-            const decimal Pi = 3.141592653m;
-            const decimal E = 2.7182818284590452m;
-            Console.WriteLine($"число Эйлера {E} число Pi {Pi}");
-            Console.ReadKey();
-        }
-        static void Task3()
-        {
-            string firstStr = "\nмоя строка1";
-            string secodStr = "\tмоя строка2";
-            string thirdStr = "\aмоя строка3";
-            Console.WriteLine(firstStr);
-            Console.WriteLine(secodStr);
-            Console.WriteLine(thirdStr);
-            Console.ReadKey();
         }
     }
 }
