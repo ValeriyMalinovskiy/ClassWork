b68908b31651a0b105391893e70a6b6dbd7d2b7f
Valer Malin <mdfcnvsn@gmail.com>
2019-04-06 11:13:54 +0300

    task1 added.

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -10,6 +10,19 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
+            Task1();
+
+            Console.ReadKey();
+        }
+
+        static void Task1()
+        {
+            int secondsPassed = Int32.Parse(Console.ReadLine());
+            //
+            //hour=3600 secs.
+            //
+            int hoursPassed = secondsPassed / 3600;
+            Console.WriteLine($"Since midnight {hoursPassed} hours passed");
         }
     }
 }
