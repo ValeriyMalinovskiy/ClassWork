ddec9e8457e7a5ccc0e01a0c13161a017f42508c
Valer Malin <mdfcnvsn@gmail.com>
2019-04-06 11:32:49 +0300

    task3 added.

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -12,7 +12,9 @@ namespace ConsoleApp
         {
             //Task1();
 
-            Task2();
+            //Task2();
+
+            Task3();
 
             Console.ReadKey();
         }
@@ -33,6 +35,17 @@ namespace ConsoleApp
             int numB = Int32.Parse(Console.ReadLine());
             int numC = Int32.Parse(Console.ReadLine());
             Console.WriteLine(((numA < numB) && (numB < numC)) ? "True" : "False");
+        }
+
+        static void Task3()
+        {
+            double inputNum = Int32.Parse(Console.ReadLine());
+            bool isFirstCond = false;
+            bool isSecondCond = false;
+            isFirstCond = ((inputNum / 1000 > 0) && (inputNum / 1000 <= 1)) ;
+            isSecondCond = (inputNum % 2 == 0);
+            if (isFirstCond & isSecondCond) Console.WriteLine("True");
+            else Console.WriteLine("False");
         }
     }
 }
