100f2258bcfe1c4ec89422eec23737228c77c171
Valer Malin <mdfcnvsn@gmail.com>
2019-04-13 14:20:37 +0300

    Task2 added

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -10,14 +10,14 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
-            Task1();
+            //Task1();
 
             Task2();
 
             Console.ReadKey();
         }
 
-        static void Task1()
+        static int[] Task1()
         {
             int size = int.Parse(Console.ReadLine());
             int[] myArr = new int[size];
@@ -28,26 +28,68 @@ namespace ConsoleApp
 
             for (int i = 0; i < myArr.Length; i++)
             {
-                myArr[i] = rnd.Next(0,101);
+                myArr[i] = rnd.Next(-10,11);
                 if (myArr[i] > maxVal)
                     maxVal = myArr[i];
                 if (myArr[i] < minVal)
                     minVal = myArr[i];
                 sum += myArr[i];
             }
-            Console.WriteLine("Average: "+(Math.Round((double)(sum / myArr.Length),2)));
-            Console.WriteLine("Max: "+ maxVal);
-            Console.WriteLine("Min: " + minVal);
-            foreach (var item in myArr)
-            {
-                if (item % 2 != 0)
-                    Console.WriteLine(item);
-            }
+            //Console.WriteLine("Average: "+(Math.Round((double)(sum / myArr.Length),2)));
+            //Console.WriteLine("Max: "+ maxVal);
+            //Console.WriteLine("Min: " + minVal);
+            //foreach (var item in myArr)
+            //{
+            //    if (item % 2 != 0)
+            //        Console.WriteLine(item);
+            //}
+            return myArr;
         }
 
         static void Task2()
         {
+            int[] arr = Task1();
+            Console.WriteLine("Sample arr:");
+            foreach (var item in arr)
+            {
+                Console.Write(item + " ");
+            }
 
+            string[] inclNum = new string[arr.Length];
+            bool isDuplicated = false;
+            int uniqueCount = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    if (arr[i] == arr[j])
+                        isDuplicated = true;
+                }
+                if (!isDuplicated)
+                {
+                    inclNum[i] = arr[i].ToString();
+                    uniqueCount++;
+                }
+                isDuplicated = false;
+            }
+
+            int[] resultArr = new int[uniqueCount];
+            uniqueCount = 0;
+            for (int i = 0; i < inclNum.Length; i++)
+            {
+                if (int.TryParse(inclNum[i], out int number))
+                {
+                    resultArr[uniqueCount] = number;
+                    uniqueCount++;
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Included numbers");
+            foreach (var item in resultArr)
+            {
+                Console.Write(item + " ");
+            }
         }
     }
 }
