3b99172acee0fa5edb187353a65c294da185ac80
Valer Malin <mdfcnvsn@gmail.com>
2019-04-13 14:34:08 +0300

    comments to Task2 added

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -49,32 +49,32 @@ namespace ConsoleApp
         static void Task2()
         {
             int[] arr = Task1();
-            Console.WriteLine("Sample arr:");
+            Console.WriteLine("Sample arr:"); //Print sample array.
             foreach (var item in arr)
             {
                 Console.Write(item + " ");
             }
 
-            string[] inclNum = new string[arr.Length];
+            string[] inclNum = new string[arr.Length]; //Create string array to fill it with non-duplicated values.
             bool isDuplicated = false;
             int uniqueCount = 0;
             for (int i = 0; i < arr.Length; i++)
             {
-                for (int j = 0; j < i; j++)
+                for (int j = 0; j < i; j++) //j<i - to search only among previously included numbers
                 {
-                    if (arr[i] == arr[j])
+                    if (arr[i] == arr[j]) 
                         isDuplicated = true;
                 }
                 if (!isDuplicated)
                 {
                     inclNum[i] = arr[i].ToString();
-                    uniqueCount++;
+                    uniqueCount++;//Counter is subsequently used to create final array.
                 }
                 isDuplicated = false;
             }
 
             int[] resultArr = new int[uniqueCount];
-            uniqueCount = 0;
+            uniqueCount = 0;//Now it is used as an index. 
             for (int i = 0; i < inclNum.Length; i++)
             {
                 if (int.TryParse(inclNum[i], out int number))
