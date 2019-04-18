5702e68f6988fc236b09a7294fe5af0fca7a802d
Valer Malin <mdfcnvsn@gmail.com>
2019-04-18 18:52:25 +0300

    Task2 added

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -10,6 +10,25 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
+            Task2();
+
+            Console.ReadKey();
+        }
+
+        static void Task2()
+        {
+            char symbol = 'o';
+            int counter = 0;
+            string str = "The string to look for the symbol";
+            Console.WriteLine(str);
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i]==symbol)
+                {
+                    counter++;
+                }
+            }
+            Console.WriteLine($"the {symbol} symbol is encountered {counter} time(s)");
         }
     }
 }
