4d7d331bcd807a9367d884fa02166839df0806a1
Valer Malin <mdfcnvsn@gmail.com>
2019-05-07 19:02:59 +0300

    Class worker added

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -10,6 +10,14 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
+            Worker w1 = new Worker("Иван", 25, 1000);
+            Worker W2 = new Worker("Вася", 26, 2000);
+            W2.Age = 0;
+
+            Console.WriteLine(w1.Salary+W2.Salary);
+            Console.WriteLine(W2.Age);
+
+            Console.ReadKey();
         }
     }
 }
---
+++ ConsoleApp/ConsoleApp/Worker.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class Worker
+    {
+        public string Name { get; set; }
+        private int age;
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+            set
+            {
+                if ((value>=1)&(value<=100))
+                {
+                    age = value;
+                }
+            }
+        }
+        public int Salary { get; set; }
+
+        public Worker(string name, int age, int salary)
+            {
+            Name = name;
+            Age = age;
+            Salary = salary;
+            }
+    }
+}
