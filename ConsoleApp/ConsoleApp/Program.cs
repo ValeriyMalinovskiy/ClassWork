1af811dec34300649ad796470642530bcb8098af
Valer Malin <mdfcnvsn@gmail.com>
2019-05-07 19:42:31 +0300

    added classes student and driver

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -10,12 +10,15 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
-            Worker w1 = new Worker("Иван", 25, 1000);
-            Worker W2 = new Worker("Вася", 26, 2000);
-            W2.Age = 0;
+            //Worker w1 = new Worker("Иван", 25, 1000);
+            //Worker W2 = new Worker("Вася", 26, 2000);
+            //W2.Age = 0;
+            //Console.WriteLine(w1.Salary+W2.Salary);
+            //Console.WriteLine(W2.Age);
 
-            Console.WriteLine(w1.Salary+W2.Salary);
-            Console.WriteLine(W2.Age);
+            Employee e1 = new Employee("Иван",25 , 1000);
+            Employee e2 = new Employee("Вася",26 ,2000);
+            Console.WriteLine(e1.Salary + e2.Salary);
 
             Console.ReadKey();
         }
---
+++ ConsoleApp/ConsoleApp/User.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class User
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public User(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+    }
+
+    class Employee : User
+    {
+        private int salary;
+        public int Salary
+        {
+            get
+            {
+                return salary;
+            }
+            set
+            {
+                if (salary > 0)
+                {
+                    salary = value;
+                }
+            }
+        }
+        public Employee(string name, int age, int salary) : base(name, age)
+        {
+            this.salary = salary;
+        }
+    }
+
+    class Student : User
+    {
+        private int scholarship;
+        private int year;
+        public int Scholarship
+        {
+            get
+            {
+                return scholarship;
+            }
+            set
+            {
+            }
+        }
+        public int Year
+        {
+            get
+            {
+                return year;
+            }
+            set
+            {
+            }
+        }
+        public Student(string name, int age, int scholarship, int year) : base(name, age)
+        {
+        }
+
+        class Driver : Worker
+        {
+            public int DrivingExperience { get; set; }
+            public DrivingLicenseCategory DrivingLicenseCategory { get; set; }
+            public Driver(string name, int age, int salary, int drivingExperience, DrivingLicenseCategory drivingLicenseCategory) : base(name, age, salary)
+            {
+
+            }
+
+        }
+
+        enum DrivingLicenseCategory
+        {
+            A,
+            B,
+            C
+        }
+    }
+
+}
