14b11d680f1499da820bd5dbfbc662f05c3ede83
Valer Malin <mdfcnvsn@gmail.com>
2019-05-11 13:23:06 +0300

    Task1 added

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Drawing;
 
 namespace ConsoleApp
 {
@@ -10,6 +11,33 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
+            Figure[] figures =
+            {
+                new Triangle(new Point(3,4), 3, 30, 45),
+                new Square(3, new Point(5,2)),
+                new Circle( new Point(6,7), 5)
+            };
+
+            foreach (var item in figures)
+            {
+                if (item.GetType()==typeof(Circle))
+                {
+                    ((Circle)item).Draw();
+                }
+                //else if (item is Triangle newTriangle)
+                //{
+                //   newTriangle.Draw();
+                //}
+                else if (item is Triangle)
+                {
+                    Triangle triangle = (Triangle)item;
+                    triangle.Draw();
+                }
+                else
+                {
+                    ((Square)item).Draw();
+                }
+            }
         }
     }
 }
---
+++ ConsoleApp/ConsoleApp/Square.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace ConsoleApp
+{
+    internal class Square : Figure
+    {
+        private int side;
+
+        public int Side
+        {
+            get
+            {
+                return this.side;
+            }
+        }
+
+        public Square(int side, Point centerCoordinates) : base(centerCoordinates)
+        {
+            this.side = side;
+        }
+
+        public void Draw()
+        {
+            Console.WriteLine($"The {this.GetType().Name} with center coordinates {CenterCoordinates.X} and {CenterCoordinates.Y} is being painted");
+        }
+    }
+}
