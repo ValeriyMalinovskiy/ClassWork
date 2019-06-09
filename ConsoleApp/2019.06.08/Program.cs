6bd2450241419ac6f20859c6ace34ba37c84565d
Valer Malin <mdfcnvsn@gmail.com>
2019-06-09 22:32:45 +0300

    Revert "Task1 recursion added"
    This reverts commit 1bd41abfdbc1fc7cf561dac94e348773a361e6c1.

--- ConsoleApp/2019.06.08/Program.cs
+++ ConsoleApp/2019.06.08/Program.cs
@@ -11,57 +11,19 @@ namespace _2019._06._08
     {
         static void Main(string[] args)
         {
-            Task1();
-
-            //Task2();
-        }
-
-        public static void Task1()
-        {
-            int folderQuantity = 0;
-            double folderSize = GetFolderNumberAndSize(@"C:\Windows\System32", ref folderQuantity) / 1073741824;
-            Console.WriteLine($"{Math.Round(folderSize, 2)} Gb");
-            Console.WriteLine($"{folderQuantity - 1} subfolders counted");
-        }
-
-        public static double GetFolderNumberAndSize(string folderPath, ref int folderQuantity)
-        {
             double totalSize = 0;
-            var directories = Directory.GetDirectories(folderPath);
-            foreach (var item in directories)
-            {
-                try
-                {
-                    totalSize += GetFolderNumberAndSize(item, ref folderQuantity);
-                }
-                catch (UnauthorizedAccessException)
-                {
-                    Console.WriteLine($"No access to {item}");
-                }
-            }
+            var pathString  = @"C:\Windows\System32";
+            var directories = Directory.GetDirectories(pathString);
+            Console.WriteLine($"there are {directories.Length} directories in {pathString}");
 
-            var files = Directory.GetFiles(folderPath);
+            string[] files = Directory.GetFiles(pathString);
+
             foreach (var item in files)
             {
                 FileInfo fi = new FileInfo(item);
                 totalSize += fi.Length;
             }
-            folderQuantity++;
-            return totalSize;
-        }
-
-        public static void Task2()
-        {
-            string pathString = @"C:\Temp\userText.txt";
-            if (true)
-            {
-
-            }
-            if (Directory.Exists(@"C:\Temp\"))
-            {
-
-            };
-            File.Create(pathString);
+            Console.WriteLine($"with the total space occupied of {totalSize}");
         }
     }
 }
