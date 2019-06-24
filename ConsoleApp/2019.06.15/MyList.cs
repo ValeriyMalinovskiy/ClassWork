db51c76de94fc8a4fe4eb9f185eba223c702c401
Valer Malin <mdfcnvsn@gmail.com>
2019-06-24 23:16:13 +0300

    Task 1 added

---
+++ ConsoleApp/2019.06.15/MyList.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2019._06._15
+{
+    public class MyList<T>
+    {
+        public int Count { get; private set; }
+
+        private T[] listItself;
+        public MyList()
+        {
+            listItself = new T[1];
+        }
+
+        public void Add(T type)
+        {
+            T[] tempList = new T[Count + 1];
+            listItself.CopyTo(tempList, 0);
+            tempList[Count] = type;
+            listItself = tempList;
+            Count++;
+        }
+
+        public T this[int index]
+        {
+            get
+            {
+                return listItself[index];
+            }
+        }
+
+        public void Clear()
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                listItself[i] = default(T);
+            }
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append(this.GetType().Name + Environment.NewLine + "Elements number: " + Count + Environment.NewLine);
+            foreach (var item in listItself)
+            {
+                sb.Append(item + " ");
+            }
+            return sb.ToString();
+        }
+
+        public bool Contains(T sample)
+        {
+            foreach (var item in listItself)
+            {
+                if (item.Equals(sample))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ ConsoleApp/2019.06.15/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2019._06._15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<int> myList = new MyList<int>();
+            myList.Add(5);
+            myList.Add(6);
+            myList.Add(7);
+            myList.Clear();
+            Console.WriteLine(myList.ToString());
+            Console.WriteLine(myList.Contains(8));
+        }
+    }
+}
