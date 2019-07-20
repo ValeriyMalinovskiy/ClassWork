fe595afd253a254a9cb3de417d98ca872f4a4f3a
Valer Malin <mdfcnvsn@gmail.com>
2019-07-20 11:31:58 +0300

    non working test

---
+++ ConsoleApp/2019.07.20/Fridge.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2019._07._20
+{
+    class Fridge
+    {
+
+        public StateEnum FridgeState { get; private set; }
+
+        public MainDoorEnum MainDoorState { get; private set; }
+
+        public FreezerEnum FreezerState { get; private set; }
+
+        public Fridge()
+        {
+            this.FridgeState = StateEnum.On;
+            this.MainDoorState = MainDoorEnum.MainDoorClosed;
+            this.FreezerState = FreezerEnum.FreezerClosed;
+        }
+
+        public delegate void FridgeStateChangedDelegate((StateEnum, MainDoorEnum, FreezerEnum) state, EventArgs eventArgs);
+        public event FridgeStateChangedDelegate FridgeEvent;
+        private void OnFridgeInvoke((StateEnum, MainDoorEnum, FreezerEnum) state, EventArgs e)
+        {
+            this.FridgeEvent?.Invoke(state, e);
+        }
+
+        public virtual void ChangeMainDoorState()
+        {
+            if (this.MainDoorState == MainDoorEnum.MainDoorClosed)
+            {
+                this.MainDoorState = MainDoorEnum.MainDoorOpened;
+            }
+            else
+            {
+                this.MainDoorState = MainDoorEnum.MainDoorClosed;
+            }
+            this.OnFridgeInvoke((this.FridgeState, this.MainDoorState, this.FreezerState), new EventArgs());
+        }
+
+        public virtual void ChangeFreezerState()
+        {
+            if (this.FreezerState == FreezerEnum.FreezerClosed)
+            {
+                this.FreezerState = FreezerEnum.FreezerOpened;
+            }
+            else
+            {
+                this.FreezerState = FreezerEnum.FreezerClosed;
+            }
+            this.OnFridgeInvoke((this.FridgeState, this.MainDoorState, this.FreezerState), new EventArgs());
+        }
+
+        public virtual void ChangeFridgeState()
+        {
+            if (this.FridgeState == StateEnum.Off)
+            {
+                this.FridgeState = StateEnum.On;
+            }
+            else
+            {
+                this.FridgeState = StateEnum.Off;
+            }
+            this.OnFridgeInvoke((this.FridgeState, this.MainDoorState, this.FreezerState), new EventArgs());
+        }
+
+        public string ShowState()
+        {
+            return this.ToString();
+        }
+
+        public override string ToString()
+        {
+            return $"The fridge is {this.FridgeState}, {this.MainDoorState}, {this.FreezerState}";
+        }
+    }
+}
---
+++ ConsoleApp/2019.07.20/FridgeEventManager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2019._07._20
+{
+    class FridgeEventManager
+    {
+        Fridge fridge = new Fridge();
+
+        public void Manage()
+        {
+            fridge.FridgeEvent += OnFridgeStateChangedInvoke;
+            Console.WriteLine("Press 1 to switch ON/OFF");
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    fridge.ChangeFridgeState();
+                    break;
+            }
+        }
+
+        private void OnFridgeStateChangedInvoke((StateEnum, MainDoorEnum, FreezerEnum) fridge1, EventArgs e)
+        {
+            fridge.ChangeFridgeState();
+        }
+
+        private void OnDoorsStateChangedInvoke(EventArgs e)
+        {
+            fridge.ChangeMainDoorState();
+        }
+    }
+}
